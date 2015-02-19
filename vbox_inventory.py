2ff68fea81229b410ad656b74a8dba20ea0d8303
Anomalous Materials <nobody@example.com>
2015-02-19 17:54:57 +0200

    Added vbox_inventory.py and wrote a readme.

---
+++ vbox_inventory.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+'''A Dynamic inventory source for Ansible.
+
+Gets a list of running VirtualBox VMs and returns it as a JSON structure.
+
+Caveats:
+ * Only running VMs are listed
+ * Only the first IP address is listed
+ * Guest additions need to be installed
+
+Reading:
+    http://docs.ansible.com/intro_inventory.html
+    http://docs.ansible.com/developing_inventory.html
+'''
+import json
+from copy import copy
+from subprocess import Popen, PIPE
+from argparse import ArgumentParser
+
+HOSTVAR_TEMPLATE = {}
+VBOX_GROUP = 'vbox'
+
+def get_running_vm_names():
+    '''Returns and iterable of VM name strings'''
+    vmlist = Popen(['VBoxManage', 'list', 'runningvms'], stdout=PIPE, stderr=PIPE)
+    sout, serr = vmlist.communicate()
+    if serr != '' or vmlist.returncode != 0:
+        raise UserWarning('VBoxManage error: %s' % serr)
+    for line in sout.splitlines():
+        yield line.split()[0].strip('"')
+        
+def get_hostvars(vmname):
+    '''Gets the host data and returns it as a dictionary.
+    
+    The primary (currently only) item is ansible_ssh_host, which is obtained
+    from VirtualBox's first IPv4 interface entry.
+    '''
+    hostvars = copy(HOSTVAR_TEMPLATE)
+    cmd = Popen(['VboxManage', 'guestproperty', 'get', vmname,
+                 '/VirtualBox/GuestInfo/Net/0/V4/IP'], stdout=PIPE, stderr=PIPE)
+    sout, serr = cmd.communicate()
+    if serr != '' or cmd.returncode != 0:
+        raise UserWarning('VBoxManage error: %s' % serr)
+    if sout != '':
+        hostvars['ansible_ssh_host'] = sout.split()[-1].strip()
+    return hostvars
+
+def vm_list_as_json():
+    '''Create JSON string containing host data'''
+    hostlist = []
+    hostvars = {}
+    for vmname in get_running_vm_names():
+        hostlist.append(vmname)
+        hostvars[vmname] = get_hostvars(vmname)
+    return json.dumps({VBOX_GROUP: hostlist, '_meta': {'hostvars': hostvars}})
+
+def main():
+    '''Command line parsing'''
+    parser = ArgumentParser(usage='%(prog)s --list')
+    parser.add_argument('--list', action='store_true', default=False,
+                        help='return VM list with host data')
+    parser.add_argument('--host', dest='vmname', help='return host data')
+    args = parser.parse_args()
+    if args.list:
+        print vm_list_as_json()
+    elif args.vmname is not None:
+        print json.dumps(get_hostvars(args.vmname))
+    else:
+        parser.print_help()
+
+if __name__ == '__main__':
+    main()
